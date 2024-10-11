ae0397292923c223dbeaf9fee1ebbbc703fa91ef
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-10-11 21:54:17 +0200

    loading datasets

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,7 +1,17 @@
 import regex
+import warnings
+
+from datasets import load_dataset
+
+def get_datasets():
+    ds_corpus = load_dataset("clarin-knext/fiqa-pl", "corpus")
+    ds_queries = load_dataset("clarin-knext/fiqa-pl", "queries")
+    return ds_corpus, ds_queries
 
 def main(): 
-    print("Hello, World!")
+    warnings.filterwarnings("ignore")
+
+    ds_corpus, ds_queries = get_datasets()
 
 if __name__ == "__main__":
     main()
